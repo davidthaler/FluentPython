acfb367e7577db5d69fdddfde72a3abbb01ae192
David Thaler <davidthaler@gmail.com>
2016-08-06 20:04:34 -0700

    added pingpong.py, a simple diamond pattern example

---
+++ ch12_inheritance/pingpong.py
@@ -0,0 +1,25 @@
+# Example 12-4 from FluentPython
+
+class A:
+    def ping(self):
+        print('ping:', self)
+
+class B(A):
+    def pong(self):
+        print('pong:', self)
+
+class C(A):
+    def pong(self):
+        print('PONG:', self)
+
+class D(B, C):
+    def ping(self):
+        super().ping()
+        print('post-ping:', self)
+
+    def pingpong(self):
+        self.ping()
+        super().ping()
+        self.pong()
+        super().pong()
+        C.pong(self)
