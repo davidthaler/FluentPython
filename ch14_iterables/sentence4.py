0523e2dfd2d3598b2bffdc5141eee1a98eee5f0f
David Thaler <davidthaler@gmail.com>
2016-08-08 20:28:51 -0700

    examples 14-5 and 14-7 done

---
+++ ch14_iterables/sentence4.py
@@ -0,0 +1,20 @@
+# Example 14-5 from Fluent Python
+# This is "sentence take #3"
+
+import re
+import reprlib
+
+RE_WORD = re.compile('\w+')
+
+class Sentence:
+
+    def __init__(self, text):
+        self.text = text
+        self.words = RE_WORD.findall(text)
+
+    def __repr__(self):
+        return 'Sentence(%s)' % reprlib.repr(self.text)
+
+    def __iter__(self):
+        for word in self.words:
+            yield word
