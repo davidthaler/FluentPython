8ec1e96ac6dca0e1b93fd3b1259f2e4b45d73b50
David Thaler <davidthaler@gmail.com>
2016-08-26 20:43:05 -0700

    added test_schedule1.py - pytest code for schedule1.py

---
+++ ch19_attributes/test_schedule1.py
@@ -0,0 +1,33 @@
+# Example A-12, the test (pytest) code for example 19-9
+
+import pytest
+import shelve
+
+import schedule1 as sched
+
+@pytest.yield_fixture
+def db():
+    with shelve.open(sched.DB) as mydb:
+        if sched.KNOWN_KEY not in mydb:
+            sched.load_db(mydb)
+        yield mydb
+
+def test_record_class():
+    rec = sched.Record(spam=99, eggs=12)
+    assert rec.spam == 99
+    assert rec.eggs == 12
+
+def test_known_key(db):
+    assert sched.KNOWN_KEY in db
+
+def test_speaker_record(db):
+    speaker = db['speaker.3471']
+    assert speaker.name == 'Anna Martelli Ravenscroft'
+
+def test_event_record(db):
+    event = db['event.33950']
+    assert event.name == 'There *Will* Be Bugs'
+
+def test_event_venue(db):
+    event = db['event.33950']
+    assert event.venue_serial == 1449
